5569bed1a704f2d3d1afafa759c3d81b6d37d8d8
al-burtsev <burtsev.aleksandr9@gmail.com>
2025-11-09 15:13:58 +0300

    fix

--- public/api/api.ts
+++ public/api/api.ts
@@ -1,4 +1,4 @@
-export const getData = async (url) => {
+export const getData = async (url: string) => {
     const res = await fetch(`${url}`, { cache: 'no-store' })
     if (!res.ok) {
         throw new Error('Failed to fetch data')
