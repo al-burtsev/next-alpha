644a7f98316b66a39be77eb55494dcfafe79d434
al-burtsev <burtsev.aleksandr9@gmail.com>
2025-11-09 15:02:49 +0300

    add fn generateStaticParams

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,4 +1,17 @@
+import { Character } from '@/src/interfaces';
 import ProductDetailsClient from './ProductDetailsClient';
+
+export async function generateStaticParams() {
+  const state = localStorage.getItem('product-storage');
+
+  if (state) {
+    const { products } = JSON.parse(state)
+
+    return products.map((product : Character) => ({
+      id: product.id.toString(),
+    }));
+  }
+}
 
 const ProductPage = async ({ params }: { params: { id: string } }) => {
   const { id } = await params;
